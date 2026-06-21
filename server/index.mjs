8ce767071af50a4ee33d6ef97e71440bbc45fb90
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2026-06-22 01:19:31 +0500

    add CORS support to allow specific origins

--- server/index.mjs
+++ server/index.mjs
@@ -4,6 +4,29 @@ import nodemailer from "nodemailer";
 
 const app = express();
 const port = Number(process.env.API_PORT || 8787);
+const allowedOrigins = [
+  "https://paktrackpro.com",
+  "https://www.paktrackpro.com",
+  "https://pak-track-pro.vercel.app",
+];
+
+app.use((request, response, next) => {
+  const origin = request.headers.origin;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    response.header("Access-Control-Allow-Origin", origin);
+    response.header("Vary", "Origin");
+  }
+
+  response.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  response.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+
+  if (request.method === "OPTIONS") {
+    return response.status(204).send();
+  }
+
+  next();
+});
 
 app.use(express.json({ limit: "1mb" }));
 
